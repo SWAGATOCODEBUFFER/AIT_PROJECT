6e664658745eab6b3d26333eaea781e6ec5cc7aa
swagatikaray <https://github.com/SWAGATOCODEBUFFER/AIT_PROJECT.git>
2022-07-17 13:35:49 +0530

    first commit

---
+++ src/main/java/in/ait/security/PasswordService.java
@@ -0,0 +1,35 @@
+package in.ait.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+	
+	
+	public static String encode(String txt)
+	{
+		Encoder encoder=Base64.getEncoder();
+		
+		return encoder.encodeToString(txt.getBytes());
+		
+	}
+	
+	public static String decode(String encodeTxt)
+	{
+		Decoder decoder=Base64.getDecoder();
+		byte[] decode=decoder.decode(encodeTxt);
+		return new String(decode);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		String encode=PasswordService.encode("swagatika");
+		System.out.println(encode);
+		String decode=PasswordService.decode(encode);
+		System.out.println(decode);
+		
+	}
+
+}
